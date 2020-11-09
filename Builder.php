0db4bf0630734dfa9459a5e88ff958dda9ef3caa
Svitavsky <vadim.svitavsky@gmail.com>
2020-11-09 20:12:02 +0300

    Добавлена поддержка практически всех необязательных параметров

--- Builder.php
+++ Builder.php
@@ -2,6 +2,21 @@
 
 class Builder
 {
+    // Возможные состояния уцененного товара
+    const CONDITION_TYPES = [
+        'likenew',    // Как новый, уценен из-за недостатков
+        'used'        // Подержанный
+    ];
+
+    // Ограничения для товара по годам
+    const AGE_YEAR = [0, 6, 12, 16, 18];
+
+    // Ограничения для товара по месяцам
+    const AGE_MONTH = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    // Максимальная длина для описания причины уценки товара
+    const CONDITION_DESCRIPTION_LENGTH = 3000;
+
     /**
      * Данные для шаблона
      * @var array
@@ -19,11 +34,22 @@ class Builder
      */
     public function build(): string
     {
+        $availableCountries = $this->getAvailableCountries();
         extract($this->data);
 
         ob_start();
         include 'template.php';
         $template = ob_get_clean();
         return $template;
+    }
+
+    /**
+     * Список доступных стран для Яндекс.Маркет
+     * @return array
+     */
+    private function getAvailableCountries()
+    {
+        $countriesList = file_get_contents('countries.csv');
+        return explode(',', $countriesList);
     }
 }
--- template.php
+++ template.php
@@ -23,14 +23,102 @@
         </categories>
         <offers>
             <?php foreach ($offers as $offer): ?>
-                <offer id="<?= $offer['id'] ?>">
+                <offer id="<?= $offer['id'] ?>" <?= isset($offer['bid']) ? "bid=\"\"" : '' ?>>
+                    <name><?= $offer['name'] ?></name>
+                    <?php if (isset($offer['vendor'])): ?>
+                        <vendor><?= $offer['vendor'] ?></vendor>
+                    <?php endif; ?>
+                    <?php if (isset($offer['vendorCode'])): ?>
+                        <vendorCode><?= $offer['vendorCode'] ?></vendorCode>
+                    <?php endif; ?>
                     <url><?= $offer['url'] ?></url>
                     <price><?= $offer['price'] ?></price>
-                    <currencyId><?= $offer['currency'] ?></currencyId>
-                    <categoryId><?= $offer['category_id'] ?></categoryId>
-                    <picture><?= $offer['picture'] ?></picture>
-                    <name><?= $offer['name'] ?></name>
+                    <?php if (isset($offer['oldprice'])): ?>
+                        <oldprice><?= $offer['oldprice'] ?></oldprice>
+                    <?php endif; ?>
+                    <enable_auto_discounts><?= $offer['enable_auto_discounts'] ?></enable_auto_discounts>
+                    <currencyId><?= $offer['currencyId'] ?></currencyId>
+                    <categoryId><?= $offer['categoryId'] ?></categoryId>
+                    <?php if (isset($offer['picture']) && strlen($offer['picture']) < 512): ?>
+                        <picture><?= $offer['picture'] ?></picture>
+                    <?php endif; ?>
+                    <?php if (isset($offer['supplier'])): ?>
+                        <supplier ogrn="<?= $offer['supplier'] ?>"/>
+                    <?php endif; ?>
+                    <?php if (isset($offer['delivery'])): ?>
+                        <delivery><?= $offer['delivery'] ?></delivery>
+                    <?php endif; ?>
+                    <?php if (isset($offer['delivery-options']) && is_array($offer['delivery-options'])): ?>
+                        <?php foreach ($offer['delivery-options'] as $option): ?>
+                            <?php $orderBefore = $option['order-before'] ? "order-before=\"{$option['order-before']}\"" : ''; ?>
+                            <option cost="<?= $option['cost'] ?>" days="<?= $option['days'] ?>" <?= $orderBefore ?>/>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($offer['pickup'])): ?>
+                        <pickup><?= $offer['pickup'] ?></pickup>
+                    <?php endif; ?>
+                    <?php if (isset($offer['pickup-options']) && is_array($offer['pickup-options'])): ?>
+                        <?php foreach ($offer['pickup-options'] as $option): ?>
+                            <?php $orderBefore = $option['order-before'] ? "order-before=\"{$option['order-before']}\"" : ''; ?>
+                            <option cost="<?= $option['cost'] ?>" days="<?= $option['days'] ?>" <?= $orderBefore ?>/>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($offer['store'])): ?>
+                        <store><?= $offer['store'] ?></store>
+                    <?php endif; ?>
                     <description><?= $offer['description'] ?></description>
+                    <!--                    <sales_notes>--><? //= $offer['sales_notes'] ?><!--</sales_notes>-->
+                    <!--                    <min-quantity>2</min-quantity>-->
+                    <?php if (isset($offer['manufacturer_warranty'])): ?>
+                        <manufacturer_warranty><?= $offer['manufacturer_warranty'] ?></manufacturer_warranty>
+                    <?php endif; ?>
+                    <?php if (isset($offer['country_of_origin']) && in_array($offer['country_of_origin'], $availableCountries)): ?>
+                        <country_of_origin><?= $offer['country_of_origin'] ?></country_of_origin>
+                    <?php endif; ?>
+                    <?php if (isset($offer['adult'])): ?>
+                        <adult><?= $offer['adult'] ?></adult>
+                    <?php endif; ?>
+                    <?php if (isset($offer['barcode']) && is_array($offer['barcode'])): ?>
+                        <?php foreach ($offer['barcode'] as $barcode): ?>
+                            <barcode><?= $offer['barcode'] ?></barcode>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($offer['param']) && is_array($offer['param'])): ?>
+                        <?php foreach ($offer['param'] as $param): ?>
+                            <?php $unit = $param['unit'] ? "order-before=\"{$param['unit']}\"" : ''; ?>
+                            <param name="<?= $param['name'] ?>" <?= $unit ?>><?= $param['value'] ?></param>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    <?php if (isset($offer['condition'], $offer['condition_description']) &&
+                        in_array($offer['condition'], self::CONDITION_TYPES) &&
+                        strlen($offer['condition_description']) <= self::CONDITION_DESCRIPTION_LENGTH): ?>
+                        <condition type="<?= $offer['condition'] ?>">
+                            <reason><?= $offer['condition_description'] ?></reason>
+                        </condition>
+                    <?php endif; ?>
+                    <?php if (isset($offer['credit-template'])): ?>
+                        <credit-template id="<?= $offer['credit-template'] ?>"/>
+                    <?php endif; ?>
+                    <?php if (isset($offer['expiry'])): ?>
+                        <expiry><?= $offer['expiry'] ?></expiry>
+                    <?php endif; ?>
+                    <?php if (isset($offer['weight'])): ?>
+                        <weight><?= $offer['weight'] ?></weight>
+                    <?php endif; ?>
+                    <?php if (isset($offer['dimensions'])): ?>
+                        <dimensions><?= $offer['dimensions'] ?></dimensions>
+                    <?php endif; ?>
+                    <?php if (isset($offer['downloadable'])): ?>
+                        <downloadable><?= $offer['downloadable'] ?></downloadable>
+                    <?php endif; ?>
+                    <?php if (isset($offer['age'])): ?>
+                        <?php if ($offer['age_type'] === 'year' && in_array($offer['age_type'], self::AGE_YEAR)): ?>
+                            <param unit="year"><?= $offer['age'] ?></param>
+                        <?php endif; ?>
+                        <?php if ($offer['age_type'] === 'month' && in_array($offer['age_type'], self::AGE_MONTH)): ?>
+                            <param unit="month"><?= $offer['age'] ?></param>
+                        <?php endif; ?>
+                    <?php endif; ?>
                 </offer>
             <?php endforeach; ?>
         </offers>
